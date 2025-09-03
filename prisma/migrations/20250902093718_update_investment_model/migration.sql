5ea0924dfe4660d2a2281ad1e578394418a4de0c
rheddi02 <rheddi02@gmail.com>
2025-09-03 09:33:33 +0800

    chore: move db to sqlite

--- prisma/migrations/20250902093718_update_investment_model/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Investment" ADD COLUMN     "date" TIMESTAMP(3);
--- prisma/migrations/20250902093953_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `date` on table `Investment` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Investment" ALTER COLUMN "date" SET NOT NULL;
